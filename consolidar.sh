685f5dd88a5f75a839d5612d38318a68188f94de
Cami-loiacono <camiloia.cole@gmail.com>
2026-03-21 03:26:30 -0300

    Fix echo command formatting in consolidar.sh

--- consolidar.sh
+++ consolidar.sh
@@ -3,5 +3,5 @@
 for archivo in entrada/*; do
     cat "$archivo" >> ~/EPNro1/salida/$FILENAME
     mv $archivo procesado
-    echo -e "\033[32mArchivo: $archivo , copiado y traladado a carpeta 'procesado'\033[0m"
+    echo -e "Archivo: $archivo , copiado y traladado a carpeta 'procesado'"
 done
